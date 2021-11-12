f0edd237eeddc51da325372af1f8f3edc79063e1
canceylandag <69530509+canceylandag@users.noreply.github.com>
2021-11-12 23:58:05 +0300

    Create Main.java

---
+++ Fraction/Main.java
@@ -0,0 +1,15 @@
+package hw5;
+
+public class Main {
+
+    public static void main(String[]args){
+    Fractional Fr = new Fractional();
+
+    Fr.Numerator=45;
+    Fr.Denumerator=72;
+
+
+    Fr.calculation(Fr.Numerator,Fr.Denumerator);
+}
+
+}
